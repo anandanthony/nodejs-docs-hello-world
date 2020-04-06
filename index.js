cf2f7dff03a06a4b172b124f2355b6a13a41c60d
anandanthony <anfranci@microsoft.com>
2020-04-06 19:14:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-    const uuidv4 = require('uuid/v4');
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(uuidv4);
     response.end("Hello World - AFT!");
 
 });
